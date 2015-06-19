510ed85ede1300e9fcfb68d2d28b058e3a9b4f29
Anurag Dutta <anuragdutta842@gmail.com>
2015-06-19 20:32:07 +0530

    Update calculator.py
    Added an option to continue using calculator after doing one operation.Earlier it just ended after doing one operation.

--- calculator.py
+++ calculator.py
@@ -3,57 +3,71 @@
 
 
 #calculator
+cmd = 0.0
+while cmd!=5 :
+	
 
-print "For addition press 1"
-print "For subtraction press 2"
-print "For multiplication press 3"
-print "For division press 4"
-print "If you're all done press 5"
+	print "For addition press 1"
+	print "For subtraction press 2"
+	print "For multiplication press 3"
+	print "For division press 4"
+	print "If you're all done press 5 \n"
 
-cmd=float(int(raw_input("Enter operation number:")))
+	cmd=float(int(raw_input("Enter operation number:")))
 
 
-#Addition
-if cmd==1:
-    print "Ok begin addition"
-    first=float(int(raw_input("Enter first number:")))
-    second=float(int(raw_input("Enter second number:")))
-    result=first+second
-    print first,"+",second,"=",result,"(sum)"
+	#Addition
+	if cmd==1:
+		print "Ok begin addition"
+		first=float(int(raw_input("Enter first number:")))
+		second=float(int(raw_input("Enter second number:")))
+		result=first+second
+		print first,"+",second,"=",result,"(sum)"
 
-#Subtraction
-elif cmd==2:
-    print "Ok begin Subtraction:"
-    first=float(int(raw_input("Enter first number:")))
-    second=float(int(raw_input("Enter second number:")))
-    result=first-second
-    print first,"-",second,"=",result,"(difference)"
+	#Subtraction
+	elif cmd==2:
+		print "Ok begin Subtraction:"
+		first=float(int(raw_input("Enter first number:")))
+		second=float(int(raw_input("Enter second number:")))
+		result=first-second
+		print first,"-",second,"=",result,"(difference)"
 
-#Multiplication
-elif cmd==3:
-    print "Ok begin multiplication"
-    first=float(int(raw_input("Enter multiplicand:")))
-    second=float(int(raw_input("Enter multiplier:")))
-    product=first*second
-    print first,"X",second,"=",product,"(product)"
+	#Multiplication
+	elif cmd==3:
+		print "Ok begin multiplication"
+		first=float(int(raw_input("Enter multiplicand:")))
+		second=float(int(raw_input("Enter multiplier:")))
+		product=first*second
+		print first,"X",second,"=",product,"(product)"
 
-#Division
-elif cmd==4:
-    print "Ok begin division"
-    print "For decimal division press 1"
-    print "For normal division press 2"
-    command=float(int(raw_input("Enter Division type:")))
-    if command==1:
-        first=float(int(raw_input("Enter Dividend:")))
-        second=float(int(raw_input("Enter Divisor:")))
-        result=first/second
-        print first,"/",second,"=",result,"(quotient)"
-    elif command==2:
-        first=int(raw_input("Enter Dividend:"))
-        second=int(raw_input("Enter Divisor:"))
-        result1=first/second
-        result2=first%second
-        print first,"/",second,"=",result1,", remainder =",result2
+	#Division
+	elif cmd==4:
+		print "Ok begin division"
+		print "For decimal division press 1"
+		print "For normal division press 2"
+		command=float(int(raw_input("Enter Division type:")))
+		if command==1:
+			first=float(int(raw_input("Enter Dividend:")))
+			second=float(int(raw_input("Enter Divisor:")))
+			result=first/second
+			print first,"/",second,"=",result,"(quotient)"
+		elif command==2:
+			first=int(raw_input("Enter Dividend:"))
+			second=int(raw_input("Enter Divisor:"))
+			result1=first/second
+			result2=first%second
+			print first,"/",second,"=",result1,", remainder =",result2
+	
+	#Exit
+	elif cmd==5:
+		break
+		
+	#Error
+	else :
+		print "Invalid input \n"
+    
+    
+
 
     
     
